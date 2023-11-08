5bf2f74cf14d4ff6417593b66baafdbd6299ed54
Davi Santos Souza <135336930+davirepost@users.noreply.github.com>
2023-11-07 22:07:40 -0300

    Update login.py

--- PI-Atelie/login.py
+++ PI-Atelie/login.py
@@ -1,13 +1,10 @@
+import tkinter as tk  # Importa o módulo tkinter e o renomeia como "tk"
+from tkinter import ttk #Importa o módulo ttk do tkinter
+import mysql.connector #Importa o módulo mysql.connector para interagir com o MySQL
 
-import tkinter as tk
-import customtkinter as Ctk
-from customtkinter import CTk
-from tkinter import ttk
-import mysql.connector
-
-
+#Função para criar a tela de login
 def tela_login():
-
+    #Estabelece a conexão com o banco de dados MySQL
     conexao = mysql.connector.connect(
         host="127.0.0.1",
         port=3306,
@@ -16,45 +13,43 @@ def tela_login():
         database='atelie'
     )
     
-    cursor = conexao.cursor()
+    cursor = conexao.cursor() #Cria um cursor para executar comandos SQL
 
+    #Função para confirmar o login
     def confirmar_login():
-        comando= "SELECT * FROM `funcionario`"
+        comando = "SELECT * FROM `funcionario`"
         cursor.execute(comando)
-        resultado= cursor.fetchall()
+        resultado = cursor.fetchall()
         conexao.commit()
-           
 
-        login=tk.Tk()
-        login.geometry("1000x600")
-        login.title ('Àrea de Login, Bem-VIndo!!')
-        texto=ttk.Label(login, text="Faça seu Login", font="Arial")
-        texto.grid(padx=10, pady=10)
-        email= ttk.Entry(login, text="Digite seu email")
-        email.grid(padx=10, pady=10)
+        login = tk.Tk() #Cria uma instância da classe Tk do tkinter para a tela de login
+        login.geometry("1000x600") #Define as dimensões da janela de login
+        login.title('Área de Login, Bem-Vindo!!') #Define o título da janela
 
-        senha= ttk.Entry(login, text="Digite sua senha", show="*")
-        senha.grid(padx=10, pady=10)
-        checkbox= ttk.Checkbutton(login, text="Lembrar Login")
-        checkbox.grid(padx=10, pady=10)
-        
-        botao= ttk.Button(login, text="Entrar", command=criar_menu)
-        botao.grid(padx=10, pady=10)
-        #botao= ttk.Button(login, text="Voltar", command=fechar)
-        #botao.grid(padx=10, pady=10)
-    
-        
-        conexao.close()
-        login.mainloop()  
+        texto = ttk.Label(login, text="Faça seu Login", font="Arial") #Cria um rótulo com um texto
+        texto.grid(padx=10, pady=10) #Coloca o rótulo na janela
 
+        email = ttk.Entry(login, text="Digite seu email") #Cria um campo de entrada para o email
+        email.grid(padx=10, pady=10) #Coloca o campo de entrada na janela
+
+        senha = ttk.Entry(login, text="Digite sua senha", show="*") #Cria um campo de entrada para a senha com visibilidade mascarada
+        senha.grid(padx=10, pady=10) #Coloca o campo de entrada na janela
+
+        checkbox = ttk.Checkbutton(login, text="Lembrar Login") #Cria uma caixa de seleção para lembrar o login
+        checkbox.grid(padx=10, pady=10) #Coloca a caixa de seleção na janela
+
+        botao = ttk.Button(login, text="Entrar", command=chamar_tela) #Cria um botão "Entrar" com a ação de chamar a tela do menu
+        botao.grid(padx=10, pady=10) #Coloca o botão na janela
+
+        conexao.close() #Fecha a conexão com o banco de dados
+        login.mainloop() #Inicia o loop principal da interface gráfica da tela de login
+
+#Função para chamar a tela do menu
 def chamar_tela():
-        #tela_login.withdraw()
-        criar_menu()
-        
+    criar_menu()
+
 def criar_menu():
-    from Menu import criar_menu
-   
+    from Menu import criar_menu  #Importa a função "criar_menu" do módulo "Menu"
 
 #def fechar():
- #   exit()
-
+#exit()
